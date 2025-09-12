c7cc2009f75c6beacfe15cd549febc614bca175f
LayorX <yor31117@gmail.com>
2025-09-12 08:48:49 +0800

    add PWA

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,47 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // No special config needed for this simple setup
-})
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+
+      devOptions: {
+        enabled: true,
+        type: 'module',
+      },
+
+      manifest: {
+        name: 'GGPoker Hand Analyzer',
+        short_name: 'ggpokerAnalyzer',
+        description: 'A completely free, privacy-focused, and lightweight web-based tool for analyzing your GGPoker hands anytime, anywhere.',
+        theme_color: '#EC4899',
+        background_color: '#0c0a18',
+        display: 'standalone',
+        orientation: 'portrait-primary',
+        start_url: '/',
+        icons: [
+          {
+            src: 'icons/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'icons/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'icons/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          }
+        ],
+      },
+    }),
+  ]
+
+});
